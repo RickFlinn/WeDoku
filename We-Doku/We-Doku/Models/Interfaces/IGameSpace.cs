f9f397515b6501fab8903c609baf2d1430fd1f4d
Tulip Pink <pinktulips@hotmail.com>
2019-03-11 13:54:45 -0700

    All crud operations

--- We-Doku/We-Doku/Models/Interfaces/IGameSpace.cs
+++ We-Doku/We-Doku/Models/Interfaces/IGameSpace.cs
@@ -7,5 +7,24 @@ namespace We_Doku.Models.Interfaces
 {
     public interface IGameSpace
     {
+        //Create 
+        Task CreateGameSpaces(GameSpaces gameSpaces);
+
+        //Delete
+        Task DeleteGameSpaces(int id);
+
+        //Read
+        Task<GameSpaces> GetGameSpaces(int? id);
+        Task <IEnumerable<GameSpaces>> GetGameSpaces();
+
+        //Update/Edit
+        Task UpdateGameSpace(GameSpaces gamespaces);
+
+        // Save
+        Task SaveAsync(GameSpaces gameSpaces);
+
+        //Check if Exists
+        bool GameSpacesExist(int id);
+
     }
 }
