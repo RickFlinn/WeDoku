c625d77b6862a1b86407e523a891053be6b9a3e3
ntibbals <ntibbals@gmail.com>
2019-03-11 16:29:42 -0700

    updated doc

--- We-Doku/We-Doku/wwwroot/js/gameplay.js
+++ We-Doku/We-Doku/wwwroot/js/gameplay.js
@@ -1,97 +1,4 @@
 ﻿
-
-//var connection = new signalR.HubConnectionBuilder().withUrl("/gamehub").build();
-
-////Disable send button until connection is established
-//document.getElementsByClassName("gsSubmit").disabled = true;
-////document.getElementById("sendButton").disabled = true;
-
-//connection.on("UpdateSpace", function (x, y) {
-//    //var msg = y.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//    //var encodedMsg = x + " says " + msg;
-//    var td = document.getElementById("" + x + y);
-//    td.css('background-color', 'red');
-//});
-
-//connection.start().then(function () {
-//    document.getElementsByClassName('gsSubmit').disabled = false;
-//    //document.getElementById("sendButton").disabled = false;
-//}).catch(function (err) {
-//    return console.error(err.toString());
-//});
-
-////function addListen() {
-////    console.log("listening");
-////    var subButt = document.getElementsByClassName('gsSubmit');
-////    console.log(subButt);
-////    for (i = 0; i < subButt.length; i++) {
-////        var item = subButt[i]fa;
-////        console.log(item);
-////        subButt[i].click(function (event) {
-////            console.log("listening222");
-
-////            var x = item.siblings().first().value;
-////            var y = item.siblings().eq(1).value;
-////            connection.invoke("SendCoordinate", x, y).catch(function (err) {
-////                return console.error(err.toString());
-////            });
-////            event.preventDefault();
-////        });
-////    };s
-////}
-//function addSignalListeners() {
-//    //let gsButton = document.getElementsByClassName('gsSubmit');
-//    //Array.from(gsButton).forEach(function (i, index) {
-//    //    console.log("hey");
-//    //    console.log(i);
-
-//    //    i.addEventListener("click", () => {
-//    //        var x = i.value();
-//    //        var y = i.value();
-
-//    //        console.log(x);
-//    //        console.log(y);
-//    //        // send x and y to hub
-//    //        connection.invoke("SendCoordinate", x, y).catch(function (err) {
-//    //            return console.error(err.toString());
-//    //        });
-//    //    });
-//    //});
-//    $('.gsSubmit').click(() => {
-//        var x = document.getElementById('1x').value;
-//        var y = document.getElementById('1y').value;
-//        console.log(x);
-//        console.log(y);
-//        // send x and y to hub
-//        connection.invoke("SendCoordinate", x, y).catch(function (err) {
-//            return console.error(err.toString());
-//        });
-//    });
-
-//}
-//$(document).ready(addSignalListeners);
-
-
-
-//document.getElementsByClassName("gsSubmit").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
-//document.getElementById("sendButton").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
-
-
-
 var connection = new signalR.HubConnectionBuilder().withUrl("/gamehub").build();
 
 //Disable send button until connection is established
@@ -150,43 +57,8 @@ connection.start().then(function () {
     return console.error(err.toString());
 });
 
-//function addListen() {
-//    console.log("listening");
-//    var subButt = document.getElementsByClassName('gsSubmit');
-//    console.log(subButt);
-//    for (i = 0; i < subButt.length; i++) {
-//        var item = subButt[i]fa;
-//        console.log(item);
-//        subButt[i].click(function (event) {
-//            console.log("listening222");
-
-//            var x = item.siblings().first().value;
-//            var y = item.siblings().eq(1).value;
-//            connection.invoke("SendCoordinate", x, y).catch(function (err) {
-//                return console.error(err.toString());
-//            });
-//            event.preventDefault();
-//        });
-//    };s
-//}
 function addSignalListeners() {
-    //let gsButton = document.getElementsByClassName('gsSubmit');
-    //Array.from(gsButton).forEach(function (i, index) {
-    //    console.log("hey");
-    //    console.log(i);
 
-    //    i.addEventListener("click", () => {
-    //        var x = i.value();
-    //        var y = i.value();
-
-    //        console.log(x);
-    //        console.log(y);
-    //        // send x and y to hub
-    //        connection.invoke("SendCoordinate", x, y).catch(function (err) {
-    //            return console.error(err.toString());
-    //        });
-    //    });
-    //});
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             let id = '' + i + j;
@@ -209,21 +81,3 @@ function addSignalListeners() {
 
 $(document).ready(addSignalListeners);
 
-
-
-//document.getElementsByClassName("gsSubmit").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
-//document.getElementById("sendButton").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
