d20fc57d1fbb84f9ea4d15bc97fcd2872bce734c
ntibbals <ntibbals@gmail.com>
2019-03-11 10:27:13 -0700

    built out chathub for testing purposes, signal r working on chat hub, need to design game hub now

---
+++ We-Doku/We-Doku/Hubs/GameHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace We_Doku.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendCoordinate(int x, int y)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", x, y);
+        }
+    }
+}
