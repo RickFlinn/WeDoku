fa6704e852ff4f4a3aeacd9a912bd480094d3e0e
Tulip Pink <pinktulips@hotmail.com>
2019-03-11 15:18:25 -0700

    game board interface

--- We-Doku/We-Doku/Models/Interfaces/IGameBoard.cs
+++ We-Doku/We-Doku/Models/Interfaces/IGameBoard.cs
@@ -7,6 +7,17 @@ namespace We_Doku.Models.Interfaces
 {
     public interface IGameBoard
     {
+        //Create 
+        Task CreateGameBoard(GameBoard gameBoard);
 
+        //Delete
+        Task DeleteGameBoard(int? id);
+
+        //Read
+        Task<GameBoard> GetGameBoard(int? id);
+        Task<IEnumerable<GameBoard>> GetGameBoard();
+
+        //Update/Edit
+        Task UpdateGameSpace(GameBoard gameBoard);
     }
 }
