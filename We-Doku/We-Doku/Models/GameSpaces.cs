cfee480a6b7f07593cefdf8c520222752ca5c62f
Richard Flinn <mriodine@gmail.com>
2019-03-11 13:39:36 -0700

    updated models for gameboard and gamespace

--- We-Doku/We-Doku/Models/GameSpaces.cs
+++ We-Doku/We-Doku/Models/GameSpaces.cs
@@ -7,5 +7,15 @@ namespace We_Doku.Models
 {
     public class GameSpaces
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Value { get; set; }
+        public bool Masked { get; set; }
+
+
+
+        // Nav properties
+        public int GameboardID { get; set; }
+        public GameBoard GameBoard { get; set; }
     }
 }
