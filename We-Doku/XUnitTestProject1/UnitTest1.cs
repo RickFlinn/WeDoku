8971e0f163ef86c2d4c60012845fa8642a668255
Richard Flinn <mriodine@gmail.com>
2019-03-14 16:49:57 -0700

    removed text dependent on unusable method

--- We-Doku/XUnitTestProject1/UnitTest1.cs
+++ We-Doku/XUnitTestProject1/UnitTest1.cs
@@ -220,35 +220,7 @@ namespace XUnitTestProject1
 
         }
 
-        [Fact]
-        public async void CanDeleteGameSpace()
-        {
-            Microsoft.EntityFrameworkCore.DbContextOptions<SudokuDbContext> options = new
-                DbContextOptionsBuilder<SudokuDbContext>().UseInMemoryDatabase
-                ("DeleteGameSpace").Options;
-
-            using (SudokuDbContext context = new SudokuDbContext(options))
-            {
-                //Arrange
-                GameSpace gameSpace = new GameSpace();
-                gameSpace.X = 1;
-                gameSpace.Y = 2;
-                gameSpace.Value = 3;
-                gameSpace.Masked = true;
-
-                gameSpace.GameBoardID = 1;
-
-                //Act
-                GameSpaceManager GSM = new GameSpaceManager(context);
-                await GSM.CreateGameSpace(gameSpace);
-                await GSM.DeleteGameSpace(1);
-
-                var result = context.GameSpaces.FirstOrDefault(g => g.X == 1 && g.Y == 2 && g.Value == 3 && g.Masked && g.GameBoardID == 1);
-
-                Assert.Null(result);
-            }
-
-        }
+        
 
         [Fact]
         public async void CanUpdateGameSpace()
